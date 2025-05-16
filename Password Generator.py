e373f5239bfb54d28e0367c85f44e4b4e14e06a7
Anagha Parameswar <anaghaparameswar22@gmail.com>
2025-05-16 19:57:58 +0530

    New program

---
+++ Password Generator.py
@@ -0,0 +1,26 @@
+import random as r
+import tkinter as tk
+chs, pw, c, l, cl = r'''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~?''', '', 0, r.randrange (8, 16), [0, 0, 0]
+def pw_generate ():
+    global pw
+    for i in range (l):
+        pw += chs [r.randrange (len (chs))]
+        ch = pw [i]
+        if ch in chs [:26]:
+            cl [0] = 1
+        elif ch in chs [52:62]:
+            cl [1] = 1
+        elif ch in chs [62:]:
+            cl [2] = 1
+pw_generate ()
+if sum (cl) != 3:
+    pw = ''
+    pw_generate ()
+print ('Your password is:', pw, sep = '\n')
+rt = tk.Tk ()
+rt.withdraw ()
+rt.clipboard_clear ()
+rt.clipboard_append (pw)
+rt.update ()
+rt.destroy ()
+print ('\nand has been copied to clipboard!')
